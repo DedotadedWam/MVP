fd93254328fb75bb2895922b1ab7918fd8817b76
Amitt Dosanjh <amitt.dosanjh@gmail.com>
2021-12-04 12:47:11 -0600

    Basic chat feature implemented and adding branch for video player with changes to server

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+// import { SocketContext } from "./SocketContext";
 
 export const VideoPlayer = () => {
-  return <div className="video-player">VideoPlayer</div>;
+  // const { name, callAccepted, myVideo, userVideo, callEnded, stream, call }} = useContext(SocketContext);
+  return (
+    <div className="video-player">
+      {/* <div>
+        <video playsInline muted ref={myVideo} autoPlay className="video" />
+      </div>
+      <div>
+        <video playsInline muted ref={userVideo} autoPlay className="video" />
+      </div> */}
+    </div>
+  );
 };
